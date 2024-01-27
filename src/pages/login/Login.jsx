ecd3ec6578e2b1eb7f5f454deec6b9798b61dcb9
Ali <chaudaryaliraza0@gmail.com>
2024-01-27 17:54:55 +0500

    add login and register pages

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,13 @@
+import "./login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+        <div className="loginWrapper">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,13 @@
+import "./register.scss";
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="registerWrapper">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Register
