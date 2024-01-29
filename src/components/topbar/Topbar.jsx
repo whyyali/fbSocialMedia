ad1c9e61755c59137e301512a509c4657c3a656d
Ali <chaudaryaliraza0@gmail.com>
2024-01-29 15:52:58 +0500

    moving router

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,43 +1,45 @@
-import { Search , Person , Chat, Notifications} from '@mui/icons-material';
+import { Search, Person, Chat, Notifications } from '@mui/icons-material';
 import "./topbar.scss";
 import Profile from '../../pages/profile/Profile';
+import { Link } from 'react-router-dom';
 
 const Topbar = () => {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
         <div className="logo">
-            <span>Facebook</span>
+          <span>Facebook</span>
         </div>
       </div>
       <div className="topbarCenter">
         <div className="searchBar">
-            <Search  className='searchBarIcon'/>
-            <input type="text" placeholder="Search your friend, photo or vedio ..." className="searchBarInput"  />
+          <Search className='searchBarIcon' />
+          <input type="text" placeholder="Search your friend, photo or vedio ..." className="searchBarInput" />
         </div>
       </div>
       <div className="topbarRight">
         <div className="topbarLinks">
-            <span className="topbarLink">Home</span>
-            <span className="topbarLink">Timeline</span>
-        </div>    
-        <div className="topbarIcons">
-            <div className="topbarIconItem">
-                <Person />
-                <span className="topbarIconBadge">1</span>
-            </div>
-            <div className="topbarIconItem">
-                <Chat />
-                <span className="topbarIconBadge">2</span>
-            </div>
-            <div className="topbarIconItem">
-                <Notifications />
-                <span className="topbarIconBadge">1</span>
-            </div>
+          <span className="topbarLink">Home</span>
+          <span className="topbarLink">Timeline</span>
         </div>
-        
-        <img src="/person/girl.jpg" alt="" className='topbarProfileImage' onClick={<Profile />} />
-      </div>   
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <Link to="/profile">
+          <img src="/person/girl.jpg" alt="" className='topbarProfileImage' />
+        </Link>
+      </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import Profile from "./pages/profile/Profile.jsx";
+import Login from "./pages/login/Login.jsx";
+import Register from "./pages/register/Register.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' >
+        <Route path='' element={<Home/>} />
+        <Route path='home' element={<Home/>} />
+        <Route path='profile' element={<Profile/>} />
+        <Route path='login' element={<Login/>} />
+        <Route path='register' element={<Register/>} />
+        <Route path='*' element={<div style={{textAlign: "center", fontSize: "20px", margin: "130px auto"}}>Not Found <h2 style={{color: "red"}}>404</h2> </div>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider  router={router}/>
   </React.StrictMode>,
 )
